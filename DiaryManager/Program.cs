054a4ad0c89cb305293a3ade98cad41ac98521ab
loiy <lloiy9310@gmail.com>
2024-06-28 13:39:07 +0300

    Add project files.

---
+++ DiaryManager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DiaryManager
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
