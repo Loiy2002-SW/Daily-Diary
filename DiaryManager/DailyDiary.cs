42de599b450749c64a5032129da0d12338f3e033
loiy <lloiy9310@gmail.com>
2024-06-28 13:56:05 +0300

    Remove unnecessary code

--- DiaryManager/DailyDiary.cs
+++ DiaryManager/DailyDiary.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Linq;
-using Microsoft.VisualBasic;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace DiaryManager
 {
