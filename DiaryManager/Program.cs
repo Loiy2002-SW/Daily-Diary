7349ff66fc58009690c1ca494aa0a6725f73eeef
loiy <lloiy9310@gmail.com>
2024-06-28 13:55:13 +0300

    Add code to the main method to start the app

--- DiaryManager/Program.cs
+++ DiaryManager/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DailyDiary diary = new DailyDiary();
+            diary.RunApp();
         }
     }
 }
